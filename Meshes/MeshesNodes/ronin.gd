fac354e558117bc4abae2dc2339ee0465cc7cd87
Miurg <rashnimorgen@gmail.com>
2024-12-19 22:45:20 +0300

    Fix schizo-code

--- Meshes/MeshesNodes/ronin.gd
+++ Meshes/MeshesNodes/ronin.gd
@@ -7,15 +7,14 @@ var curAnim = IDLE
 
 @export var blend_speed = 15 
 
-var run_val = 0
-var walk_val = 0
-var attackrun_val = 0
-var die_val = 0
+var run_val:float = 0
+var walk_val:float = 0
+var attackrun_val:float = 0
+var die_val:float = 0
 
 func _physics_process(delta):
 	handle_animations(delta)
-	
-	curAnim = RUN
+	update_tree()
 
 func handle_animations(delta):
 	match curAnim:
@@ -33,7 +32,7 @@ func handle_animations(delta):
 			run_val = lerpf(run_val,0, blend_speed*delta)
 			walk_val = lerpf(walk_val,1, blend_speed*delta)
 			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
-			die_val = lerpf(die_val,0, blend_speed*delta)	
+			die_val = lerpf(die_val,0, blend_speed*delta)
 		ATTACKRUN:
 			run_val = lerpf(run_val,0, blend_speed*delta)
 			walk_val = lerpf(walk_val,0, blend_speed*delta)
@@ -43,11 +42,11 @@ func handle_animations(delta):
 			run_val = lerpf(run_val,0, blend_speed*delta)
 			walk_val = lerpf(walk_val,0, blend_speed*delta)
 			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
-			die_val = lerpf(die_val,1, blend_speed*delta)	
+			die_val = lerpf(die_val,1, blend_speed*delta)
 
 func update_tree():
-	animation_tree["parameters/Run/blend_amount"] = run_val
-	animation_tree["parameters/Walk/blend_amount"] = walk_val
-	animation_tree["parameters/AttackRun/blend_amount"] = attackrun_val
-	animation_tree["parameters/Die/blend_amount"] = die_val
+	animation_tree["parameters/RunBlend/blend_amount"] = run_val
+	animation_tree["parameters/WalkBlend/blend_amount"] = walk_val
+	animation_tree["parameters/AttackRunBlend/blend_amount"] = attackrun_val
+	animation_tree["parameters/DieBlend/blend_amount"] = die_val
 	

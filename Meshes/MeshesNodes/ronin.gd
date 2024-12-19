9f29a085b62a54a75a533125ded1e5f48cacd129
Hiro <empiratories@gmail.com>
2024-12-19 20:58:54 +0300

    Update katana and anim tree

--- Meshes/MeshesNodes/ronin.gd
+++ Meshes/MeshesNodes/ronin.gd
@@ -1,11 +1,53 @@
 extends Node3D
 
+enum {IDLE,WALK,RUN,ATTACKRUN,DIE}
+var curAnim = IDLE
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var animation_tree: AnimationTree = $AnimationTree
 
+@export var blend_speed = 15 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var run_val = 0
+var walk_val = 0
+var attackrun_val = 0
+var die_val = 0
+
+func _physics_process(delta):
+	handle_animations(delta)
+	
+	curAnim = RUN
+
+func handle_animations(delta):
+	match curAnim:
+		IDLE:
+			run_val = lerpf(run_val,0, blend_speed*delta)
+			walk_val = lerpf(walk_val,0, blend_speed*delta)
+			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
+			die_val = lerpf(die_val,0, blend_speed*delta)
+		RUN: 	
+			run_val = lerpf(run_val,1, blend_speed*delta)
+			walk_val = lerpf(walk_val,0, blend_speed*delta)
+			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
+			die_val = lerpf(die_val,0, blend_speed*delta)
+		WALK:
+			run_val = lerpf(run_val,0, blend_speed*delta)
+			walk_val = lerpf(walk_val,1, blend_speed*delta)
+			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
+			die_val = lerpf(die_val,0, blend_speed*delta)	
+		ATTACKRUN:
+			run_val = lerpf(run_val,0, blend_speed*delta)
+			walk_val = lerpf(walk_val,0, blend_speed*delta)
+			attackrun_val = lerpf(attackrun_val,1, blend_speed*delta)
+			die_val = lerpf(die_val,0, blend_speed*delta)
+		DIE:
+			run_val = lerpf(run_val,0, blend_speed*delta)
+			walk_val = lerpf(walk_val,0, blend_speed*delta)
+			attackrun_val = lerpf(attackrun_val,0, blend_speed*delta)
+			die_val = lerpf(die_val,1, blend_speed*delta)	
+
+func update_tree():
+	animation_tree["parameters/Run/blend_amount"] = run_val
+	animation_tree["parameters/Walk/blend_amount"] = walk_val
+	animation_tree["parameters/AttackRun/blend_amount"] = attackrun_val
+	animation_tree["parameters/Die/blend_amount"] = die_val
+	
